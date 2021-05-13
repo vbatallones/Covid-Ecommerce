5b055df96a11ca23e3241779b8e8abac85f9d8a7
Alice Huh <aklee213@gmail.com>
2021-05-12 22:02:19 -0400

    connect to local host

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 8000;
-const passport = require('passport');
+const passport = require("passport");
 
+//MIDDLEWARE
 app.use(cors());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// Passport Middleware
+//passport middleware
 app.use(passport.initialize());
-// Importing passport file into server 
-require('./config/passport')(passport);
+//importing passport file into server
+// require("./config/passport")(passport);
 
 
-
-app.get('/', (req, res)=> {
-    res.status(200).json({ message: `Alice's Backend!!`})
+//ROUTES
+app.get("/", (req, res) =>
+{
+    res.status(200).json({message: "Smile, you\'re being watched by the Backend Team"});
 });
 
-app.listen(port, () => {
+app.listen(port, () =>
+{
     console.log(`Server is running on port: ${port}`);
-})
+});
